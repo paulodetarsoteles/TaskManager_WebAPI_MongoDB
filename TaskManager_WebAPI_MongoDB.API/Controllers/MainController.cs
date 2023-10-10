bb508e98e97ef2d1c60ce5333dd413b899c70b66
Paulo de Tarso <potassio@hotmail.com>
2023-10-10 19:57:29 -0300

    Adição da classe MainController

---
+++ TaskManager_WebAPI_MongoDB.API/Controllers/MainController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager_WebAPI_MongoDB.API.Controllers
+{
+    [Route("apiV1/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        // GET: api/<MainController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MainController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MainController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MainController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MainController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

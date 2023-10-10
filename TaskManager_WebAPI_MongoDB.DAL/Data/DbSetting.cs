830626a83fc08fff8aecdbe2efd0b61d9cade4c6
Paulo de Tarso <potassio@hotmail.com>
2023-10-10 20:38:40 -0300

    Instalação do Nuget MongoDB.Driver e inicialização da classe DbSetting

---
+++ TaskManager_WebAPI_MongoDB.DAL/Data/DbSetting.cs
@@ -0,0 +1,11 @@
+﻿using TaskManager_WebAPI_MongoDB.DAL.Data.Interfaces;
+
+namespace TaskManager_WebAPI_MongoDB.DAL.Data
+{
+    internal class DbSetting : IDbSetting
+    {
+        public string DbName { get; } = string.Empty;
+
+        public string DbConnectionString { get; } = string.Empty;
+    }
+}
---
+++ TaskManager_WebAPI_MongoDB.DAL/Data/Interfaces/IDbSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager_WebAPI_MongoDB.DAL.Data.Interfaces
+{
+    public interface IDbSetting
+    {
+        string DbName { get; }
+        string DbConnectionString { get; }
+    }
+}

9a2cdfe3117f6517122ace703db69ff7e2408118
Paulo de Tarso <potassio@hotmail.com>
2023-10-10 20:24:37 -0300

    Criação da interface e do repositório

--- TaskManager_WebAPI_MongoDB.DAL/Repositories/Interfaces/ITaskRepository.cs
+++ TaskManager_WebAPI_MongoDB.DAL/Repositories/Interfaces/ITaskRepository.cs
@@ -2,5 +2,10 @@
 {
     public interface ITaskRepository
     {
+        IEnumerable<Task> GetAll();
+        Task GetByName(string name);
+        void Insert(Task task);
+        void Update(string taskId, Task task);
+        void Delete(string taskId);
     }
 }
---
+++ TaskManager_WebAPI_MongoDB.DAL/Repositories/TaskRepository.cs
@@ -0,0 +1,32 @@
+﻿using TaskManager_WebAPI_MongoDB.DAL.Repositories.Interfaces;
+
+namespace TaskManager_WebAPI_MongoDB.DAL.Repositories
+{
+    internal class TaskRepository : ITaskRepository
+    {
+        public IEnumerable<Task> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string taskId, Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string taskId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

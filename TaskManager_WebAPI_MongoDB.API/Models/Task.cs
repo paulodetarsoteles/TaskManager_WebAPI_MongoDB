e62675fdec2e45c3336fc7525b23cb5508d275d1
Paulo de Tarso <potassio@hotmail.com>
2023-10-10 20:13:23 -0300

    Criação da model de tarefas (Task)

---
+++ TaskManager_WebAPI_MongoDB.API/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskManager_WebAPI_MongoDB.API.Models
+{
+    public class Task
+    {
+        public Task(string name, int? value)
+        {
+            TaskId = Guid.NewGuid().ToString();
+            Name = name;
+            Value = value;
+            Done = false;
+            CreationDate = DateTime.Now;
+            UpdateDate = null;
+        }
+
+        public string TaskId { get; private set; } = string.Empty;
+        public string Name { get; private set; } = string.Empty;
+        public int? Value { get; private set; }
+        public bool Done { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
